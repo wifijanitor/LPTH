43ef29ad081204a3c4715eb67c85240065cd632d
wifijanitor <steve@wifijanitor.com>
2017-05-16 15:11:08 -0500

    stuff

---
+++ ex2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# comments
+'''
+or commetns like this
+'''
+
+print ("I could do it like this") # commetns are ignored
+
+# print ("This won't run")
+
+print ("This will run")
