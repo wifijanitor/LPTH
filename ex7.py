c04e46710b8c7deb3357ef672ba437ffd5a0b18f
wifijanitor <steve@wifijanitor.com>
2017-05-22 11:00:23 -0500

    more

--- ex7.py
+++ ex7.py
@@ -19,5 +19,5 @@ end11 = "e"
 end12 = "r"
 
 # watch that comma at the end.  try removing it to see what happens
-print (end1 + end2 + end3 + end4 + end5 + end6)
-print (end7 + end8 + end9 + end10 + end11 + end12)
+print (end1 + end2 + end3 + end4 + end5 + end6 \
+	end7 + end8 + end9 + end10 + end11 + end12)
